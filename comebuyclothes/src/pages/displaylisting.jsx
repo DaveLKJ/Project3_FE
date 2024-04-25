1bded7ca33cc9af18e1980e55d9c3d4d435b5775
Tan Mei Kee <meikee@Tans-MacBook-Air.local>
2024-04-25 09:47:50 +0800

    connected the pages tgt

---
+++ comebuyclothes/src/pages/displaylisting.jsx
@@ -0,0 +1,11 @@
+import ListingItem from "../Components/ListingItem"
+
+function DisplayListing(){
+    return(
+        <>
+        DisplayListing - calls all the items in warehouse
+        <ListingItem/>
+        </>
+    )
+}
+export default DisplayListing
--- comebuyclothes/src/pages/home.jsx
+++ comebuyclothes/src/pages/home.jsx
@@ -1,3 +1,17 @@
 // this home.jsx should have a navbar component at the top for user's navigation
 // a random image for the looks lol
 // 'footer' component at the bottom
+
+import Navbar from "../Components/Navbar"
+
+
+function Home(){
+    return(
+        <>
+        <Navbar/>
+        Home
+        </>
+    )
+}
+export default Home
+
