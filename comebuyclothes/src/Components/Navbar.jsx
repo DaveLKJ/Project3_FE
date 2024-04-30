3c1303d5abc5e1a650f42c94cc1a68ec7fa935fa
Hui Lun C <huilunc@Huis-MacBook-Air.local>
2024-04-30 23:28:59 +0800

    updated pages and components and appjs

--- comebuyclothes/src/Components/Navbar.jsx
+++ comebuyclothes/src/Components/Navbar.jsx
@@ -1,8 +1,53 @@
-function Navbar(){
-    return(
-        <nav>
-        Navbar
-        </nav>
-    )
-}
-export default Navbar
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.css"
+import { Link } from "react-router-dom";
+
+
+
+const Navbar = () => {
+
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+
+          <div className="item">
+            <Link className ="link" to="/products/1">Cat 1</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/2">Cat 2</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/3">Cat 3</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className ="link" to="/">comebuyclothes</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className ="link" to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon/>
+            <PersonOutlineOutlinedIcon/>
+            <FavoriteBorderOutlinedIcon/>
+            <div className="cartIcon" >
+                <Link to="/ShoppingCart">
+                    <ShoppingCartOutlinedIcon/>
+                    <span>0</span>
+                    </Link>    
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
