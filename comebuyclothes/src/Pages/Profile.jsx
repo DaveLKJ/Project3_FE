51bb80b65a3a771950d5ea8d1e3ebc5f36df316b
Hui Lun C <huilunc@Huis-MacBook-Air.local>
2024-05-02 22:50:43 +0800

    added profile

---
+++ comebuyclothes/src/Pages/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My Details</h1>
+      <h2>Name</h2>
+      <h2>Email</h2>
+      <h2>Add Your Mobile Number!</h2>
+      <h2>Add Your Birthday!</h2>
+      <h2>Add Your Address!</h2>
+      
+      <h1>Password</h1>
+      <h2>Edit Password, don't verify just edit</h2>
+    </div>
+  )
+}
+
+export default Profile
