84d199743a11054ffd0de09084dd97aeb48973cf
Tan Mei Kee <meikee@Tans-MacBook-Air.local>
2024-05-07 23:35:55 +0800

    added Checkout Page

---
+++ comebuyclothes/src/Pages/Payment.jsx
@@ -0,0 +1,20 @@
+function Payment (props){
+
+    const {cart} = props
+    return(
+        <div>
+            You are seeing Payment Page
+            {cart.map((item)=>{
+                return (
+                    <div>
+                        <h1>{item.name}</h1>
+                        <p>{item.quantity}</p>
+                        <p>total price: ${(item.price)*(item.quantity)}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Payment
